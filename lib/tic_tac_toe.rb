fddaf1f5d864c5d9f71a8b5ed8f4ac5ecf69181b
Sarah Rose <sarahrose63089@gmail.com>
2020-02-29 00:28:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,8 +117,8 @@ def play(board) #repeat turn until over returns true
   until over?(board)
     turn(board)
       print board
-    over?(board)
-      # print false
+    # over?(board)
+    false  # print false
   end
   if won?(board)
     winner = winner(board)
